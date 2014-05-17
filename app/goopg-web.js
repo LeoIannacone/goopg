bd7de8da9ac9206dfcdc81206d16fb6ac2ee22b2
Leo Iannacone <l3on@ubuntu.com>
2014-05-17 18:13:48 +0200

    communication between webpage and extestion

--- app/goopg-web.js
+++ app/goopg-web.js
@@ -1,5 +1,52 @@
-var goopg_web = document.createElement('div');
-goopg_web.id = "goopg";
-goopg_web.style.display = "none";
-goopg_web.innerHTML = GLOBALS[9]
-document.getElementsByTagName('body')[0].appendChild(goopg_web);
+GOOPG_CLASS_PREFIX = "goopg-"
+GOOPG_CLASS_CHECKED = GOOPG_CLASS_PREFIX + "checked"
+
+function get_orig_message(id) {
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    var auth = GLOBALS[9];
+    console.log(auth)
+    url = "https://mail.google.com/mail/u/0/?ui=2&ik=" + auth + "&view=om&th=" + id
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function check_message(id) {
+    info = {}
+    info.id = id
+    info.message = get_orig_message(id)
+    chrome.runtime.sendMessage('ddlebbablilfigfkkjedbpapjichmjgd', info,
+        function(response) {
+            console.log("Received " + response);
+        });
+}
+
+function look_for_messages() {
+    messages = document.getElementsByClassName('ii');
+    for (var i = 0; i < messages.length; i++) {
+        var id = null;
+        var classList = messages[i].classList;
+        if (classList.contains(GOOPG_CLASS_CHECKED))
+            continue;
+        for (var j = 0; j < classList.length; j++) {
+            if (classList[j].length > 5 && classList[j][0] == 'm') {
+                id = classList[j].substring(1);
+                messages[i].classList.add(GOOPG_CLASS_CHECKED)
+                break;
+            }
+        }
+        if (id)
+            check_message(id)
+    }
+}
+
+// var divs = document.getElementsByTagName('div')
+// for (var i = 0; i < divs.length; i++) {
+//     if (divs[i].getAttribute('role') == 'main') {
+//         divs[i].addEventListener('DOMSubtreeModified', look_for_messages, false);
+//         break;
+//     }
+// }
+
+document.body.addEventListener('DOMSubtreeModified', look_for_messages, false);
--- app/goopg.js
+++ app/goopg.js
@@ -1,52 +1,5 @@
-GOOPG_CLASS_PREFIX = "goopg-"
-GOOPG_CLASS_CHECKED = GOOPG_CLASS_PREFIX + "checked"
-
-function get_orig_message(id) {
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    var auth = document.getElementById('goopg').innerHTML;
-    url = "https://mail.google.com/mail/u/0/?ui=2&ik=" + auth + "&view=om&th=" + id
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
-
-function check_message(id) {
-    console.log(get_orig_message(id))
-    // info = {}
-    // info.id = id
-    // info.message = get_orig_message(id)
-    // chrome.runtime.sendNativeMessage('com.leoiannacone.goopg', info.message,
-    //     function(response) {
-    //         console.log("Received " + response);
-    //     });
-}
-
-function look_for_messages() {
-    messages = document.getElementsByClassName('ii');
-    for (var i = 0; i < messages.length; i++) {
-        var id = null;
-        var classList = messages[i].classList;
-        if (classList.contains(GOOPG_CLASS_CHECKED))
-            continue;
-        for (var j = 0; j < classList.length; j++) {
-            if (classList[j].length > 5 && classList[j][0] == 'm') {
-                id = classList[j].substring(1);
-                messages[i].classList.add(GOOPG_CLASS_CHECKED)
-                break;
-            }
-        }
-        if (id)
-            check_message(id)
+chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+        console.log(request, sender, sendResponse)
     }
-}
-
-// var divs = document.getElementsByTagName('div')
-// for (var i = 0; i < divs.length; i++) {
-//     if (divs[i].getAttribute('role') == 'main') {
-//         divs[i].addEventListener('DOMSubtreeModified', look_for_messages, false);
-//         break;
-//     }
-// }
-
-document.body.addEventListener('DOMSubtreeModified', look_for_messages, false);
+);
