7c3786e7d6d97846b6462abdc5af8ffb17777c55
Leo Iannacone <l3on@ubuntu.com>
2014-05-18 15:03:43 +0200

    verify sign and alert user

--- app/goopg-web.js
+++ app/goopg-web.js
@@ -1,6 +1,66 @@
 GOOPG_CLASS_PREFIX = "goopg-"
 GOOPG_CLASS_CHECKED = GOOPG_CLASS_PREFIX + "checked"
 
+function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function(s) {
+        return {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': '&quot;',
+            "'": '&#39;',
+            "/": '&#x2F;'
+        }[s];
+    });
+}
+
+String.prototype.capitalize = function() {
+    return this.replace(/(?:^|\s)\S/g, function(a) {
+        return a.toUpperCase();
+    });
+};
+
+function toggleDisplay(div) {
+    if (div.style.display == "none")
+        div.style.display = "block";
+    else
+        div.style.display = "none"
+}
+
+function showGPGstdErr(div) {
+    toggleDisplay(div.parentElement.getElementsByClassName('gpgStdErr')[0])
+}
+
+function build_alert(msg) {
+    var className;
+    var text;
+    var extra_button = '';
+    if (msg.status == null)
+        return;
+    else if (msg.status == "no public key") {
+        className = "warning";
+        text = "public key " + msg.stderr.match(/using .* key ID (.*)/)[1] + " not found"
+    } else if (msg.status == "signature valid") {
+        className = "success";
+        text = msg.username + " " + msg.stderr.match(/using .* key ID (.*)/)[0]
+    } else { // (msg.status == "signature bad")
+        className = "danger";
+        text = msg.username + " " + msg.stderr.match(/using .* key ID (.*)/)[0]
+    }
+    // clean stderr
+    var stderr = msg.stderr.replace(/^.GNU.*\n?/mg, '');
+    var result = document.createElement('div');
+    result.className = "goopg";
+    result.innerHTML = '<div class="alert alert-' + className + '">' +
+        '<a onclick="showGPGstdErr(this)" class="btn btn-link alert-link pull-right">' +
+        '<span class="glyphicon glyphicon glyphicon-info-sign"></span></a>' +
+        '<strong>' + msg.status.capitalize() + ':</strong> ' + escapeHtml(text) +
+        '<div class="gpgStdErr raw" style="display:none;">' + escapeHtml(stderr) + '</div>' +
+        '</div>';
+    return result;
+}
+
+
 var goopgExtensionId = "ddlebbablilfigfkkjedbpapjichmjgd";
 var port = chrome.runtime.connect(goopgExtensionId);
 
@@ -10,6 +70,16 @@ port.onDisconnect.addListener(function() {
 
 port.onMessage.addListener(function(msg) {
     console.log("Received", msg);
+    if (msg.status == null)
+        return;
+    var div = document.getElementsByClassName("m" + msg.id)[0]
+    // for (var i = 0; i < div.children.length; i++)
+    //     div.children[i].style.display = "none";
+    div.insertBefore(build_alert(msg), div.firstChild);
+    // var body = document.createElement('div')
+    // body.className = "raw"
+    // body.innerHTML = escapeHtml(msg.data)
+    // div.appendChild(body)
 });
 
 function get_orig_message(id) {
@@ -24,6 +94,7 @@ function get_orig_message(id) {
 
 function check_message(id) {
     info = {}
+    info.command = "verify"
     info.id = id
     info.message = get_orig_message(id)
     port.postMessage(info)
