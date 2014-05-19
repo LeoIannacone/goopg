5574bdf27c8684135f9456a6e225e84272b5b283
Leo Iannacone <l3on@ubuntu.com>
2014-05-19 15:42:02 +0200

    unified code

--- host/gpg_wrapper.py
+++ host/gpg_wrapper.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
-from gnupg import GPG
 import email
 import gpgmail
-
-gpg = GPG()
 
 
 def _get_decoded_payload(message):
@@ -19,25 +16,12 @@ def _get_decoded_payload(message):
 def verify(data):
     result = {}
     message = email.message_from_string(data)
-    if message.get_content_type() == 'multipart/mixed':
-        message = message.get_payload()[0]
-    if message.get_content_type() == 'multipart/signed':
-        for verified, contents in gpgmail.signed_parts(message):
-            if verified is not None:
-                result = verified.__dict__
-                if not 'data' in result or not result['data']:
-                    result['data'] = _get_decoded_payload(contents)
-                else:
-                    # get the real content, strip message Headers
-                    msg_tmp = email.message_from_string(result['data'])
-                    result['data'] = _get_decoded_payload(msg_tmp)
-                break
-    else:
-        result = gpg.verify(data).__dict__
+    for verified, contents in gpgmail.signed_parts(message):
+        if verified is not None:
+            result = verified.__dict__
+            break
     if 'gpg' in result:
         del(result['gpg'])
-    if 'key_id' in result and result['key_id'] is None:
-        result['data'] = data
     return result
 
 
@@ -55,8 +39,8 @@ if __name__ == '__main__':
     def pJSON(d):
         print json.dumps(d, indent=4)
 
-    # pJSON(verify(open('../nosign.txt', 'rb').read()))
+    pJSON(verify(open('../nosign.txt', 'rb').read()))
     pJSON(verify(open('../sign.inline.txt', 'rb').read()))
-    # pJSON(verify(open('../sign.attached.txt', 'rb').read()))
-    # pJSON(verify(open('../sign.attached2.txt', 'rb').read()))
-    # pJSON(verify(open('/tmp/clean', 'rb').read()))
+    pJSON(verify(open('../sign.attached.txt', 'rb').read()))
+    pJSON(verify(open('../sign.attached2.txt', 'rb').read()))
+    pJSON(verify(open('/tmp/clean', 'rb').read()))
--- host/gpgmail.py
+++ host/gpgmail.py
@@ -1,8 +1,10 @@
 # -*- mode:python; coding:utf-8; -*- Time-stamp: <gpgmail.py - root>
 # copyright (c) konstantin.co.uk. all rights reserved.
-# Origin: https://code.google.com/p/python-gnupg/issues/attachmentText?id=4&aid=7243301884964405207&name=gpgmail.py&token=2e585ecf0d148cb330041b1b00d769ff
+# Origin: https://code.google.com/p/python-gnupg/issues/
+#         attachmentText?id=4&aid=7243301884964405207&name=
+#         gpgmail.py&token=2e585ecf0d148cb330041b1b00d769ff
 
-__all__ = ['filter_parts', 'flatten', 'signed_parts',]
+__all__ = ['filter_parts', 'flatten', 'signed_parts']
 
 import re
 
@@ -10,61 +12,79 @@ from cStringIO import StringIO
 from email.generator import Generator
 from gnupg import GPG
 
-def _armor(container, message, signature) :
-    if container.get_param('protocol') == 'application/pgp-signature' :
+
+def _armor(container, message, signature):
+    if container.get_param('protocol') == 'application/pgp-signature':
         m = re.match(r'^pgp-(.*)$', container.get_param('micalg'))
 
-        if m :
-            TEMPLATE = '-----BEGIN PGP SIGNED MESSAGE-----\nHash: %s\n\n%s\n%s\n'
+        if m:
+            TEMPLATE = '-----BEGIN PGP SIGNED MESSAGE-----\n' \
+                       'Hash: %s\n\n' \
+                       '%s\n%s\n'
             s = StringIO()
             text = re.sub(r'(?m)^(-.*)$', r'- \1', flatten(message))
 
-            s.write(TEMPLATE % (m.group(1).upper(), text, signature.get_payload()))
-
+            s.write(TEMPLATE % (m.group(1).upper(),
+                                text,
+                                signature.get_payload()))
             return s.getvalue()
-
     return None
+
 
 def filter_parts(m, f):
     """Iterate over messages that satisfy predicate."""
-    for x in m.walk() :
-        if f(x) :
+    for x in m.walk():
+        if f(x):
             yield x
 
-def flatten(message) :
+
+def flatten(message):
     """Return raw string representation of message."""
-    try :
+    try:
         s = StringIO()
-        g = Generator(s, mangle_from_ = False, maxheaderlen = 0)
+        g = Generator(s, mangle_from_=False, maxheaderlen=0)
 
         g.flatten(message)
         return s.getvalue()
-
-    finally :
+    finally:
         s.close()
 
-def signed_parts(message) :
-    """Iterate over signed parts of message yielding GPG verification status and signed contents."""
-    ARMOR_RE = re.compile(r'^-+BEGIN PGP SIGNED MESSAGE-+\n(?:.*\n)+\n((?:.*\n)+)-+BEGIN PGP SIGNATURE-+\n(?:.*\n)+-+END PGP SIGNATURE-+\s*$')
 
-    f = lambda m : m.is_multipart() and m.get_content_type() == 'multipart/signed' or not m.is_multipart() and m.get_content_maintype() == 'text'
+def signed_parts(message):
+    """Iterate over signed parts of message yielding
+    GPG verification status and signed contents."""
+
+    f = lambda m: \
+        m.is_multipart() and m.get_content_type() == 'multipart/signed' \
+        or not m.is_multipart() and m.get_content_maintype() == 'text'
+
     gpg = GPG()
 
-    for part in filter_parts(message, f) :
-        if part.is_multipart() :
-            try :
+    for part in filter_parts(message, f):
+        if part.is_multipart():
+            try:
                 signed_part, signature = part.get_payload()
-                s = _armor(part, signed_part, signature) if signature.get_content_type() == 'application/pgp-signature' else None
-
-                if s :
+                s = None
+                if signature.get_content_type() == 'application/pgp-signature':
+                    s = _armor(part, signed_part, signature)
                     yield gpg.verify(s), signed_part
 
-            except ValueError :
+            except ValueError:
                 pass
 
-        else :
-            payload = part.get_payload(decode = True)
-            m = ARMOR_RE.match(payload)
-
-            if m :
-                yield gpg.verify(payload), m.group(1)
+        else:
+            payload = part.get_payload(decode=True)
+            gpg_regex = [r'^-+BEGIN PGP SIGNED MESSAGE-+\s+(.*\s+)',
+                         r'^-+BEGIN PGP SIGNATURE-+\s+(.*\s+)',
+                         r'^-+END PGP SIGNATURE-+\s+(.*\s+)']
+            try:
+                for reg in gpg_regex:
+                    if not re.compile(reg, re.M).search(payload):
+                        raise ValueError
+                    signed_part = re.compile(gpg_regex[0],
+                                             re.M).split(payload)[-1]
+                    signed_part = re.compile(gpg_regex[1],
+                                             re.M).split(signed_part)[0]
+                    yield gpg.verify(payload), signed_part
+            except (ValueError, IndexError):
+                yield gpg.verify(payload), payload
