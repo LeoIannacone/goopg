44e6b6029536ecd89ef9b38fe17b777a2be354c6
Leo Iannacone <l3on@ubuntu.com>
2014-10-30 01:44:36 +0100

    add send&sign button

--- app/goopg-web.js
+++ app/goopg-web.js
@@ -2,22 +2,13 @@
 var GOOPG_CLASS_PREFIX = "goopg-";
 var GOOPG_CLASS_CHECKED = GOOPG_CLASS_PREFIX + "checked";
 var GOOPG_CLASS_STDERR = GOOPG_CLASS_PREFIX + "stderr";
+var GOOPG_CLASS_SENDBUTTON = GOOPG_CLASS_PREFIX + "sendbutton";
 
 var GOOGLE_CLASS_MESSAGE = "ii";
-var GOOGLE_CLASS_CONTROLS = "IZ";
-
-function escapeHtml(string) {
-    return String(string).replace(/[&<>"'\/]/g, function (s) {
-        return {
-            "&": "&amp;",
-            "<": "&lt;",
-            ">": "&gt;",
-            "\"": "&quot;",
-            "'": "&#39;",
-            "/": "&#x2F;"
-        }[s];
-    });
-}
+//var GOOGLE_CLASS_CONTROLS = "IZ";
+var GOOGLE_CLASS_SENDBUTTON = "aoO";
+var GOOGLE_CLASS_DISCARDBUTTON = 'og';
+var GOOGLE_CLASS_ALERTMSG = 'vh';
 
 String.prototype.capitalize = function () {
     return this.replace(/(?:^|\s)\S/g, function (a) {
@@ -25,88 +16,117 @@ String.prototype.capitalize = function () {
     });
 };
 
-function toggleDisplay(div) {
-    if (div.style.display == "none")
-        div.style.display = "block";
-    else
-        div.style.display = "none";
-}
+var Utils = {
 
-function hide_signature(filename, div) {
-    var body = div.firstChild.innerHTML;
-    if (filename === null) {
-        // try to hide inline signature
-        var info = body.split(/^-+BEGIN PGP SIGNED MESSAGE-+.*\n.*\n<br>\n+/m);
-        info = info[info.length - 1];
-        info = info.split(/^-+BEGIN PGP SIGNATURE-+<br>\n/m)[0];
-        if (info.length != div.firstChild.innerHTML.length) {
-            div.firstChild.innerHTML = info;
-            return true;
-        }
-    } else {
-        // here only if signature is attached
-        var spans = div.parentElement.getElementsByTagName("span");
-        for (var i = 0; i < spans.length; i++) {
-            var span = spans[i];
-            var download_url = span.getAttribute("download_url");
-            if (download_url &&
-                download_url.indexOf("text/plain:" + filename) === 0) {
-                // this is the sign attachment
-                span.style.display = "none";
-                if (span.parentElement.children.length == 2) {
-                    // only one attachment, hide the whole box
-                    span.parentElement.parentElement.style.display = "none";
-                }
+    escapeHtml: function (string) {
+        return String(string).replace(/[&<>"'\/]/g, function (s) {
+            return {
+                "&": "&amp;",
+                "<": "&lt;",
+                ">": "&gt;",
+                "\"": "&quot;",
+                "'": "&#39;",
+                "/": "&#x2F;"
+            }[s];
+        });
+    },
+
+    toggleDisplay: function (div) {
+        if (div.style.display == "none")
+            div.style.display = "block";
+        else
+            div.style.display = "none";
+    },
+
+    hide_signature: function (filename, div) {
+        var body = div.firstChild.innerHTML;
+        if (filename === null) {
+            // try to hide inline signature
+            var info = body.split(/^-+BEGIN PGP SIGNED MESSAGE-+.*\n.*\n<br>\n+/m);
+            info = info[info.length - 1];
+            info = info.split(/^-+BEGIN PGP SIGNATURE-+<br>\n/m)[0];
+            if (info.length != div.firstChild.innerHTML.length) {
+                div.firstChild.innerHTML = info;
                 return true;
             }
+        } else {
+            // here only if signature is attached
+            var spans = div.parentElement.getElementsByTagName("span");
+            for (var i = 0; i < spans.length; i++) {
+                var span = spans[i];
+                var download_url = span.getAttribute("download_url");
+                if (download_url &&
+                    download_url.indexOf("text/plain:" + filename) === 0) {
+                    // this is the sign attachment
+                    span.style.display = "none";
+                    if (span.parentElement.children.length == 2) {
+                        // only one attachment, hide the whole box
+                        span.parentElement.parentElement.style.display = "none";
+                    }
+                    return true;
+                }
+            }
         }
-    }
-    return false;
-}
+        return false;
+    },
 
-function build_alert(msg) {
-    var className;
-    var text;
-    var icon;
-    if (msg.status === null)
-        return;
-    else if (msg.status == "no public key") {
-        icon = "question-sign";
-        className = "warning";
-        text = "public key " + msg.stderr.match(/using .* key ID (.*)/)[1] + " not found";
-    } else if (msg.status == "signature valid") {
-        icon = "ok-sign";
-        className = "success";
-        text = msg.username + " " + msg.stderr.match(/using .* key ID (.*)/)[0];
-    } else { // (msg.status == "signature bad")
-        icon = "exclamation-sign";
-        className = "danger";
-        text = msg.username + " " + msg.stderr.match(/using .* key ID (.*)/)[0];
-    }
-    // clean stderr
-    var stderr = msg.stderr.replace(/^.GNUPG:.*\n?/mg, "");
-    var result = document.createElement("div");
-    result.className = "goopg";
-    var alert = document.createElement("div");
-    alert.className = "alert alert-" + className;
-    var alert_header = document.createElement("div");
-    alert_header.className = "alert-header";
-    alert_header.addEventListener("click", function () {
-        toggleDisplay(this.parentElement.getElementsByClassName(GOOPG_CLASS_STDERR)[0]);
-    });
-    alert_header.innerHTML =
-        "<span class=\"pull-right glyphicon glyphicon glyphicon-" + icon + "\"></span>" +
-        "<strong>" + msg.status.capitalize() + ":</strong> " + escapeHtml(text);
-    var alert_stderr = document.createElement("div");
-    alert_stderr.className = "raw " + GOOPG_CLASS_STDERR;
-    alert_stderr.style.display = "none";
-    alert_stderr.innerHTML = escapeHtml(stderr);
+    build_alert: function (msg) {
+        var className;
+        var text;
+        var icon;
+        if (msg.status === null)
+            return;
+        var key_id = msg.key_id;
+        if (key_id.length > 8)
+            key_id = msg.key_id.substring(8);
+        text = msg.username + " " + key_id;
+        if (msg.status == "no public key") {
+            icon = "question-sign";
+            className = "warning";
+            text = "public key " + key_id + " not found";
+        } else if (msg.status == "signature valid" || msg.status == "signature good") {
+            icon = "ok-sign";
+            className = "success";
+        } else { // (msg.status == "signature bad")
+            icon = "exclamation-sign";
+            className = "danger";
+        }
+        // clean stderr
+        var result = document.createElement("div");
+        result.className = "goopg";
+        var alert = document.createElement("div");
+        alert.className = "alert alert-" + className;
+        var alert_header = document.createElement("div");
+        alert_header.className = "alert-header";
+        alert_header.innerHTML =
+            "<span class=\"pull-right glyphicon glyphicon glyphicon-" + icon + "\"></span>" +
+            "<strong>" + msg.status.capitalize() + ":</strong> " + Utils.escapeHtml(text);
+        alert.appendChild(alert_header);
+        if (msg.stderr) {
+            var stderr = msg.stderr.replace(/^.GNUPG:.*\n?/mg, "");
+            alert_header.addEventListener("click", function () {
+                Utils.toggleDisplay(this.parentElement.getElementsByClassName(GOOPG_CLASS_STDERR)[0]);
+            });
+            var alert_stderr = document.createElement("div");
+            alert_stderr.className = "raw " + GOOPG_CLASS_STDERR;
+            alert_stderr.style.display = "none";
+            alert_stderr.innerHTML = Utils.escapeHtml(stderr);
+            alert.appendChild(alert_stderr);
+        }
 
-    alert.appendChild(alert_header);
-    alert.appendChild(alert_stderr);
-    result.appendChild(alert);
-    return result;
-}
+        result.appendChild(alert);
+        return result;
+    },
+
+    build_sendbutton: function () {
+        var new_button = document.createElement('div');
+        new_button.className = GOOPG_CLASS_SENDBUTTON;
+        new_button.id += GOOPG_CLASS_PREFIX + Math.random();
+        //new_button.className += ' T-I-JW';
+        new_button.innerHTML = 'Sign and Send';
+        return new_button;
+    }
+};
 
 
 
@@ -124,11 +144,13 @@ function get_web_port() {
 
     port.onMessage.addListener(function (msg) {
         window.console.log("Received", msg);
-        if (msg.result.status === null)
-            return;
-        var div = document.getElementsByClassName("m" + msg.id)[0];
-        hide_signature(msg.result.filename, div);
-        div.insertBefore(build_alert(msg.result), div.firstChild);
+        if (msg.command == "verify") {
+            if (msg.result.status === null)
+                return;
+            var div = document.getElementsByClassName("m" + msg.id)[0];
+            Utils.hide_signature(msg.result.filename, div);
+            div.insertBefore(Utils.build_alert(msg.result), div.firstChild);
+        }
     });
 
     return port;
@@ -143,7 +165,7 @@ function send_message_web_port(message) {
     }
 }
 
-function look_for_messages() {
+function look_for_signedmessages() {
     var messages = document.getElementsByClassName(GOOGLE_CLASS_MESSAGE);
     for (var i = 0; i < messages.length; i++) {
         var id = null;
@@ -166,4 +188,41 @@ function look_for_messages() {
     }
 }
 
-document.body.addEventListener("DOMSubtreeModified", look_for_messages, false);
+function on_click_sendsignbutton() {
+    var e = this;
+    // get the message id, in html: <input name="draft" value="MSG_ID" />
+    while (e.parentElement) {
+        e = e.parentElement;
+        var inputs = e.getElementsByTagName('input');
+        for (var j = 0; j < inputs.length; j++) {
+            var input = inputs[j];
+            if (input.getAttribute('name') == "draft") {
+                var info = {};
+                info.command = "sign";
+                info.id = input.getAttribute('value');
+                info.button_id = this.id;
+                send_message_web_port(info);
+                //console.log(info)
+                return;
+            }
+        }
+    }
+}
+
+function look_for_compositors() {
+    var sendButtons = document.getElementsByClassName(GOOGLE_CLASS_SENDBUTTON);
+    // append the sign&send button
+    for (var i = 0; i < sendButtons.length; i++) {
+        var button = sendButtons[i];
+        var parent = button.parentNode;
+        if (parent.className.indexOf(GOOPG_CLASS_CHECKED) > -1)
+            continue;
+        parent.className += ' ' + GOOPG_CLASS_CHECKED;
+        var new_button = Utils.build_sendbutton(button);
+        new_button.addEventListener("click", on_click_sendsignbutton);
+        parent.appendChild(new_button);
+    }
+}
+
+document.body.addEventListener("DOMSubtreeModified", look_for_signedmessages, false);
+document.body.addEventListener("DOMSubtreeModified", look_for_compositors, false);
