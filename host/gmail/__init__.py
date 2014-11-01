a60bc83c4156bc2f39432c259213c0f173f586a5
Leo Iannacone <l3on@ubuntu.com>
2014-11-01 13:29:10 +0100

    refactory to refresh credentials before re-login, fix smtp connection expired and login fail

--- host/gmail/__init__.py
+++ host/gmail/__init__.py
@@ -18,56 +18,75 @@ from multiprocessing import Process, Queue
 from xdg import BaseDirectory
 
 
+# Check https://developers.google.com/gmail/api/auth/scopes
+# for all available scopes
+OAUTH_SCOPE = 'https://mail.google.com/'
+
+# Path to the client_secret.json, file comes from the Google Developer Console
+CLIENT_SECRET_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                  'client_secret.json')
+
+# Directory where the credentials storage files are placed
+STORAGE_DIR = BaseDirectory.save_cache_path(os.path.join('goopg', 'storage'))
+
+
 class GMail():
 
     def __init__(self, username):
         # the main username
         self.username = username
-
-        # Path to the client_secret.json file
-        # downloaded from the Developer Console
-        current_path = os.path.dirname(os.path.abspath(__file__))
-        CLIENT_SECRET_FILE = os.path.join(current_path, 'client_secret.json')
-
-        # Check https://developers.google.com/gmail/api/auth/scopes
-        # for all available scopes
-        OAUTH_SCOPE = 'https://mail.google.com/'
-
-        # Location of the credentials storage file
-        cache_dir = os.path.join('goopg', 'storage')
-        cache_dir = BaseDirectory.save_cache_path(cache_dir)
-        STORAGE = Storage(os.path.join(cache_dir, username))
+        self.http = httplib2.Http()
 
         # Start the OAuth flow to retrieve credentials
-        flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=OAUTH_SCOPE)
-        http = httplib2.Http()
+        flow = flow_from_clientsecrets(CLIENT_SECRET_FILE,
+                                       scope=OAUTH_SCOPE)
+
+        # The storage for current user
+        storage = Storage(os.path.join(STORAGE_DIR, self.username))
 
         # Try to retrieve credentials from storage
         # or run the flow to generate them
-        self.credentials = STORAGE.get()
-
+        self.credentials = storage.get()
         if self.credentials is None or self.credentials.invalid:
             # call a subprocess as workaround for stdin/stdout
-            # blocked by the main process
+            # blocked by the main process, this is the main function:
+            def _subprocess_login():
+                queue.put(run(flow, storage, http=self.http))
+
+            # A Queue to get the result from subprocess
             queue = Queue()
-            p = Process(target=_login, args=(queue, flow, STORAGE, http))
+            p = Process(target=_subprocess_login)
             p.start()
             p.join()
+            # Retreive the credentials
             self.credentials = queue.get()
 
-        # Authorize the httplib2.Http object with our credentials
-        http = self.credentials.authorize(http)
-
-        # Build the Gmail service from discovery
-        self.gmail_service = build('gmail', 'v1', http=http)
-        self.messages = self.gmail_service.users().messages()
-        self.drafts = self.gmail_service.users().drafts()
+        # Access to the GMail APIs
+        self._gmail_API_login()
 
         # Use smtp as workaround to send message, GMail API
         # bugged with Content-Type: multipart/signed
         self._smtp_login()
 
+    def _gmail_API_login(self):
+        self._refresh_credentials()
+
+        # Authorize the httplib2.Http object with our credentials
+        authorized_http = self.credentials.authorize(self.http)
+
+        # Build the Gmail service from discovery
+        self.gmail_service = build('gmail', 'v1', http=authorized_http)
+        self.messages = self.gmail_service.users().messages()
+        self.drafts = self.gmail_service.users().drafts()
+
+    def _refresh_credentials(self):
+        """Refresh credentials if needed"""
+        if self.credentials.access_token_expired:
+            self.credentials.refresh(self.http)
+
     def _smtp_login(self):
+        self._refresh_credentials()
+
         # intialize SMTP procedure
         self.smtp = SMTP('smtp.gmail.com', 587)
 
@@ -82,7 +101,6 @@ class GMail():
                                                    access_token)
         smpt_auth_b64 = base64.b64encode(smtp_auth_string)
         # smtp login
-        smpt_auth_b64
         self.smtp.docmd("AUTH", "XOAUTH2 {}".format(smpt_auth_b64))
         self.smtp.send("\r\n")
 
@@ -115,8 +133,3 @@ class GMail():
         except SMTPServerDisconnected:
             self._smtp_login()
             self.smtp.sendmail(sender, receiver, message.as_string())
-
-
-
-def _login(queue, flow, storage, http):
-    queue.put(run(flow, storage, http=http))
